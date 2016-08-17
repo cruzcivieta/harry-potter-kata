5a383d56c59feb518afa6ed85383dadfc11d192b
Daniel Cruz <=>
2016-08-17 14:04:57 +0200

    Refactoring

--- src/CruzCivieta/HarryPotterCalculator.php
+++ src/CruzCivieta/HarryPotterCalculator.php
@@ -4,39 +4,68 @@ namespace CruzCivieta;
 
 class HarryPotterCalculator
 {
+    const DISCOUNTS = [
+        1 => 0,
+        2 => 0.95,
+        3 => 0.90,
+        4 => 0.80,
+    ];
+
+
+    const NORMAL_PRICE = 8.00;
+
     public function calculate($books)
     {
-        if (count($books) === 2 && $books[0] !== $books[1]) {
-            return $this->toMoneyRepresentation('15.20');
+        $differentBooks = array_unique($books);
+        if (count($differentBooks) > 1) {
+            $total = $this->calculateWithDiscount($differentBooks);
+            $total += $this->calculateRest($books, $differentBooks);
+
+            return $this->toMoneyRepresentation((string) $total);
         }
 
-        if (count($books) === 3) {
-            $differentBooks = array_unique($books);
-
-            if (count($differentBooks) === 2) {
-                return $this->toMoneyRepresentation('23.20');
-            }
-
-            if (count($differentBooks) === 3) {
-                return $this->toMoneyRepresentation('21.60');
-            }
-        }
-
-        if (count($books) === 4) {
-            $differentBooks = array_unique($books);
-
-            if (count($differentBooks) === 3) {
-                return $this->toMoneyRepresentation('29.60');
-            }
-
-            return $this->toMoneyRepresentation('25.60');
-        }
-
-        return $this->toMoneyRepresentation((string) (8.00 * count($books)));
+        return $this->toMoneyRepresentation((string) $this->calculateBuyWithDifferentBooks($books));
     }
 
     private function toMoneyRepresentation($amount)
     {
         return [$amount, 'EUR'];
+    }
+
+    /**
+     * @param $differentBooks
+     * @return float
+     */
+    private function calculateWithDiscount($differentBooks)
+    {
+        return round(static::NORMAL_PRICE * count($differentBooks) * $this->retrieveDiscount($differentBooks), 2);
+    }
+
+    /**
+     * @param $books
+     * @param $differentBooks
+     * @return int
+     */
+    private function calculateRest($books, $differentBooks)
+    {
+        return static::NORMAL_PRICE * (count($books) - count($differentBooks));
+    }
+
+    /**
+     * @param $books
+     * @return float
+     */
+    private function calculateBuyWithDifferentBooks($books)
+    {
+        return static::NORMAL_PRICE * count($books);
+    }
+
+    /**
+     * @param $differentBooks
+     * @return mixed
+     */
+    private function retrieveDiscount($differentBooks)
+    {
+        return static::DISCOUNTS[count($differentBooks)];
     }
 }
